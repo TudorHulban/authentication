941f9d24cdb1bf0ab23a4c263551258e164dd614
Tudor Hulban <prindex2010@gmail.com>
2024-06-19 16:28:57 +0300

    introducing ProcessFormURLEncoded

---
+++ helpers/url_encoded.go
@@ -0,0 +1,71 @@
+package helpers
+
+// id=&status=&name=aa
+
+func ProcessFormURLEncoded(response string) map[string]string {
+	result := make(map[string]string)
+
+	var buffer []rune
+
+	var modeKey, modeValue bool
+
+	var key, value string
+
+	runeEqual := []rune("=")
+	runeAmpersand := []rune("&")
+
+	for ix, digit := range []rune(response) {
+		if ix == 0 {
+			modeKey = true
+		}
+
+		if digit == runeEqual[0] {
+			if modeKey {
+				key = string(buffer)
+
+				modeKey = false
+				modeValue = true
+				buffer = []rune{}
+			}
+
+			continue
+		}
+
+		if digit == runeAmpersand[0] {
+			if modeValue {
+				value = string(buffer)
+
+				modeKey = true
+				modeValue = false
+				buffer = []rune{}
+
+				if len(key) > 0 && len(value) > 0 {
+					result[key] = value
+				}
+
+				key = ""
+				value = ""
+			}
+
+			continue
+		}
+
+		if ix == len([]rune(response))-1 {
+			if modeValue {
+				buffer = append(buffer, digit)
+
+				value = string(buffer)
+
+				if len(key) > 0 && len(value) > 0 {
+					result[key] = value
+				}
+			}
+
+			break
+		}
+
+		buffer = append(buffer, digit)
+	}
+
+	return result
+}
---
+++ helpers/url_encoded_test.go
@@ -0,0 +1,37 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestErrorsProcessFormURLEncoded(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       string
+		want        map[string]string
+	}{
+		{"1. error", "=", map[string]string{}},
+		{"2. error", "&", map[string]string{}},
+		{"3. error", "=&", map[string]string{}},
+		{"4. error", "&=", map[string]string{}},
+		{"5. error invalid field", "aa=&", map[string]string{}},
+		{"6. error invalid field", "=aa&", map[string]string{}},
+		{"7. one value", "aa=bb&", map[string]string{"aa": "bb"}},
+		{"8. one value", "aa=bb", map[string]string{"aa": "bb"}},
+		{"9. empty values", "id=&status=&name=", map[string]string{}},
+		{"10. several values", "id=&status=&aa=bb", map[string]string{"aa": "bb"}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description,
+			func(t *testing.T) {
+				assert.Equal(t,
+					tc.want,
+					ProcessFormURLEncoded(tc.input),
+				)
+			},
+		)
+	}
+}
